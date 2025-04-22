d3d607e8ec7fa61f552cc98343463f6883194892
Ryann <ryanngohenrui@gmail.com>
2025-04-21 21:36:21 -0700

    Add autopairs

---
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "windwp/nvim-autopairs",
+    lazy = false,
+    event = "InsertEnter",
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+  },
+}
