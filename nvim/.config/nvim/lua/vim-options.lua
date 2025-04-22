2fd20e59d0fec00c7e55308f9a6570d5f9eee32e
Ryann <ryanngohenrui@gmail.com>
2025-04-21 21:53:54 -0700

    Update vim

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -23,3 +23,9 @@ vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 vim.wo.number = true
+
+-- Window resizing (new mappings)
+-- Increase width
+vim.keymap.set("n", "<leader>+", ":vertical resize +5<CR>", { noremap = true })
+-- Decrease width
+vim.keymap.set("n", "<leader>-", ":vertical resize -5<CR>", { noremap = true })
